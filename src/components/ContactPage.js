23552f5d4fb91cedf8de7c20cd5829aec12133c1
TiannaLopes <tiannaalinalopes@gmail.com>
2024-05-08 19:39:14 -0700

    Adding space scene to projects page and to contact page with data displayed

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SpaceScene from './SpaceScene';
+import '../styles/shared.css';
 
 function ContactPage() {
   return (
-    <div className='container'>
-      <h1>Contact Me</h1>
-      <p>Feel free to get in touch for collaborations or questions!</p>
+    <div className='contact-container'>
+      <SpaceScene />
+      <div className='contact-content'>
+        <h1>Contact Me</h1>
+        <p>Feel free to get in touch for collaborations or questions!</p>
+      </div>
     </div>
   );
 }
--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import SpaceScene from './SpaceScene';
 import projectsData from '../data/projects.json';
+import '../styles/shared.css';
 
 function ProjectsPage() {
   return (
-    <div className='container'>
-      <h1>Projects</h1>
-      {projectsData.map((project) => (
-        <div key={project.id}>
-          <h3>{project.name}</h3>
-          <p>{project.description}</p>
-        </div>
-      ))}
+    <div className='projects-container'>
+      <div className='projects-list'>
+        <h1>Projects</h1>
+        {projectsData.map((project) => (
+          <div key={project.id} className='project-card'>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
+      <SpaceScene />
     </div>
   );
 }
